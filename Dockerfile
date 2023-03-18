e276e5b11c3fd3426cdc5632dfaba1ca8d2f1efb
Igor <fuumanze@gmail.com>
2023-03-18 21:16:48 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.10-slim
+
+WORKDIR /code
+
+COPY requirements.txt .
+RUN pip install --no-cache -r requirements.txt
+
+COPY . .
+
+CMD gunicorn run:app -b 0.0.0.0:5000 -w 4
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from main.views import main_blueprint
+from errorhandler.errorhandler import error_handler
+from api.api import api_blueprint
+
+def create_app():
+    app = Flask(__name__)
+    app.config['JSON_AS_ASCII'] = False
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(error_handler)
+    app.register_blueprint(api_blueprint)
+
+    return app
